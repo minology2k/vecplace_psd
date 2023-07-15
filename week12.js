255813511cea7e57fb3ea38adcec62efc485c044
duyensherry <dinhhueduyen@gmail.com>
2023-07-15 16:26:07 +0700

    exercise_week_12

--- week12.js
+++ week12.js
@@ -1,1 +1,25 @@
-a
+var logos = document.querySelectorAll(".logo");
+var headerLinks = document.querySelectorAll(".header__nav-item a");
+var showcaseSearchInput = document.querySelector(".banner__form-input");
+var showcaseSearchButton = document.querySelector(".banner__form-submit");
+var showcaseBackground = document.querySelector(".banner__image img");
+var sponsorBrands = document.querySelectorAll(".sponsor__list li");
+var popularCityImage = document.querySelectorAll(".popular__item img");
+var mostVisitLocations = document.querySelectorAll(".popular__desc");
+var memberCards = document.querySelectorAll(".member__info img");
+var bookNowButton = document.querySelector(".button--normal button");
+var footerLinks = document.querySelectorAll(".footer__list-item");
+
+console.log({
+    logos,
+    headerLinks,
+    showcaseSearchInput,
+    showcaseSearchButton,
+    showcaseBackground,
+    sponsorBrands,
+    popularCityImage,
+    mostVisitLocations,
+    memberCards,
+    bookNowButton,
+    footerLinks
+})
